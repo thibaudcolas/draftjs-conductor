52a33b33eadc0940d0c9db8f7eebe59ff0e9f25a
Thibaud Colas <thibaudcolas@gmail.com>
2018-06-03 21:23:11 +0300

    chore(demo): fix highlight component taking too long to render

--- src/demo/components/Highlight.js
+++ src/demo/components/Highlight.js
@@ -1,38 +1,29 @@
-// @flow
+import PropTypes from "prop-types";
 import React from "react";
-import Prism from "prismjs";
-
-type Props = {
-  value: string,
-  language: string,
-};
 
 const onCopy = (value) => {
   const hidden = document.createElement("textarea");
   hidden.value = value;
-  // $FlowFixMe
   document.body.appendChild(hidden);
   hidden.select();
   document.execCommand("copy");
-  // $FlowFixMe
   document.body.removeChild(hidden);
 };
 
-const Highlight = ({ value, language }: Props) => (
-  <pre className={`language-${language}`} style={{ position: "relative" }}>
+const Highlight = ({ value }) => (
+  <pre style={{ position: "relative" }}>
     <button
       onClick={onCopy.bind(null, value)}
       style={{ position: "absolute", right: "1rem" }}
     >
       Copy
     </button>
-    <code
-      // eslint-disable-next-line springload/react/no-danger
-      dangerouslySetInnerHTML={{
-        __html: Prism.highlight(value, Prism.languages[language]),
-      }}
-    />
+    <code>{value}</code>
   </pre>
 );
+
+Highlight.propTypes = {
+  value: PropTypes.string.isRequired,
+};
 
 export default Highlight;
