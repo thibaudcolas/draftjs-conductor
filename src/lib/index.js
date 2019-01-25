cd9f531dfbad5d8532c60a02f358450614af700e
Thibaud Colas <thibaudcolas@gmail.com>
2019-01-25 05:16:16 +0200

    style(lists): switch ListNestingStyles to named export internally

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,19 +1,12 @@
 // @flow
 
-import ListNestingStyles, {
-  DRAFT_DEFAULT_MAX_DEPTH,
-  DRAFT_DEFAULT_DEPTH_CLASS,
-  generateListNestingStyles,
-  blockDepthStyleFn,
-} from "./components/ListNestingStyles";
-
 export {
   ListNestingStyles,
   DRAFT_DEFAULT_MAX_DEPTH,
   DRAFT_DEFAULT_DEPTH_CLASS,
   generateListNestingStyles,
   blockDepthStyleFn,
-};
+} from "./components/ListNestingStyles";
 
 export {
   registerCopySource,
