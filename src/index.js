6b9a2434702207a5344001a62e29455d47d294c5
Thibaud Colas <thibaudcolas@gmail.com>
2019-08-13 21:25:00 +0100

    chore(demo): fix demo site not serving due to Webpack tree shaking

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 // @flow
-import "./demo/index";
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./demo/utils/elements.css";
+import "./demo/utils/typography.css";
+import "./demo/utils/layout.css";
+import "./demo/utils/objects.css";
+
+import "draft-js/dist/Draft.css";
+
+import "./demo/components/header.css";
+import "./demo/components/page-nav.css";
+
+import "./demo/utils/utilities.css";
+
+import App from "./demo/components/App";
+
+const mount = document.getElementById("root");
+
+if (mount) {
+  ReactDOM.render(<App />, mount);
+}
